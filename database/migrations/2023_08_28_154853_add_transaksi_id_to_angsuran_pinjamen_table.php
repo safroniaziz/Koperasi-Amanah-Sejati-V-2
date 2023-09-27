ea5c85c5d8e1590d6fbff27003a4debd8b887b75
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-09-27 11:43:28 +0700

    new commit

--- database/migrations/2023_08_28_154853_add_transaksi_id_to_angsuran_pinjamen_table.php
+++ database/migrations/2023_08_28_154853_add_transaksi_id_to_angsuran_pinjamen_table.php
@@ -14,9 +14,6 @@ return new class extends Migration
         Schema::table('angsuran_pinjamen', function (Blueprint $table) {
             $table->date('tanggal_transaksi')->change();
             $table->string('bulan_transaksi')->change();
-
-            $table->dropForeign(['jenis_transaksi_id']);
-            $table->dropColumn('jenis_transaksi_id');
         });
     }
 
@@ -26,15 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('angsuran_pinjamen', function (Blueprint $table) {
-            $table->dropForeign(['transaksi_id']);
-            $table->dropColumn('transaksi_id');
-
             $table->integer('tanggal_transaksi')->change();
             $table->integer('bulan_transaksi')->change();
-
-            $table->unsignedBigInteger('jenis_transaksi_id');
-            $table->foreign('jenis_transaksi_id')->references('id')->on('jenis_transaksis');
-
         });
     }
 };
