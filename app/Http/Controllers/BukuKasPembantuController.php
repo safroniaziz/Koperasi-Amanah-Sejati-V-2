c30e1003b1eadbaf5946cc825da55a34f1bdf001
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-10-17 16:49:59 +0700

    new commit

--- app/Http/Controllers/BukuKasPembantuController.php
+++ app/Http/Controllers/BukuKasPembantuController.php
@@ -38,10 +38,9 @@ class BukuKasPembantuController extends Controller
             $modalAwal = ModalAwal::where('tahun',$request->tahun)->where('bulan',$request->bulan)->first();
 
             $transaksis = TransaksiKoperasi::with(['jenisTransaksi'])
-                                            ->whereYear('tanggal_transaksi',$request->tahun)
-                                            ->whereMonth('tanggal_transaksi',$request->bulan)
+                                            ->where('tahun_transaksi',$request->tahun)
+                                            ->whereMonth('bulan_transaksi',$request->bulan)
                                             ->orderBy('tanggal_transaksi','asc')
-                                            ->distinct()
                                             ->get();
             return view('backend.bukuKasPembantu.index',[
                 'bulan' =>  $request->bulan,
