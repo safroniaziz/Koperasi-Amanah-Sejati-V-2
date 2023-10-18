aacb7f5ff165bc4580730d1bae459f3f94cb63b0
Safroni Aziz Suprianto <40552260+safroniaziz@users.noreply.github.com>
2023-10-18 22:52:19 +0700

    new commit

--- resources/views/backend/angsuran/create.blade.php
+++ resources/views/backend/angsuran/create.blade.php
@@ -37,15 +37,17 @@
             <div class="form-group col-md-6">
                 <label for="exampleInputEmail1">Jumlah Angsuran Jasa / Bulan</label>
                 @php
-                    if ($pinjaman->jumlah_bulan == 24) {
-                        $jasa = round($pinjaman->angsuran_pokok * (16/100));
-                    }else{
-                        $jasa = round($pinjaman->angsuran_pokok * (8/100));
-                    }
-                @endphp
-                <input type="text" name="angsuran_jasa" 
-                        value="{{ $jasa }}"
-                id="angsuran_bunga" class="form-control">
+if ($pinjaman->jumlah_bulan == 24) {
+    $jasa = round($pinjaman->angsuran_pokok * (16/100));
+} else {
+    $jasa = round($pinjaman->angsuran_pokok * (8/100));
+}
+@endphp
+
+<input type="text" name="angsuran_jasa" 
+    value="{{ number_format($jasa, 0, '', '') }}"
+    id="angsuran_bunga" class="form-control">
+
             </div>
         </div>
     </div>
